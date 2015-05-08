9f3cd6934368dd8bf2f7e056a0a39c0cfa233bd8
Abhishek Ahirwar <abhiahirwar@gmail.com>
2015-05-08 15:33:47 +1000

    fix static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+	describe "GET home" do
+		it "renders the home template" do
+			get "home"
+
+			expect(response).to render_template("home")
+		end
+	end
+
+	describe "GET Help" do 
+		it "renders the help template" do
+			get "help"
+			expect(response).to render_template("help")
+		end
+	end
+
 end
