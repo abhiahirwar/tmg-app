79cc6aa7e4efcb64b7ef2cccbc866de1d4e52ade
Abhishek Ahirwar <abhiahirwar@gmail.com>
2015-05-10 18:32:18 +1000

    fix production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,17 +83,18 @@ Rails.application.configure do
 
   config.assets.initialize_on_precompile = false
 
-  config.action_mailer.default_url_options = { host: 'tmg-app.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: "tmg-app.herokuapp.com" }
   
   ENV["GMAIL_USERNAME"] = 'abhiindiaus@gmail.com'
   ENV["GMAIL_PASSWORD"] = 'abh1shek'
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "mail.google.com",####important
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.google.com",####important
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
