46119b46c1741bd6fab3a0e981a4887e5111fdc2
Abhishek Ahirwar <abhiahirwar@gmail.com>
2015-06-23 00:11:02 +1000

    fix success page

--- app/mailers/enrolment_mailer.rb
+++ app/mailers/enrolment_mailer.rb
@@ -1,7 +1,7 @@
 class EnrolmentMailer < ApplicationMailer
 	default from: 'noreply@tmg.edu.au'
   default bcc: "abhiindiaus@gmail.com"
-  default bcc: "tmgit@edu.au"
+  default bcc: "tmgit@tmg.edu.au"
 
 	def welcome_email(user)
     @user = user
@@ -13,6 +13,6 @@ class EnrolmentMailer < ApplicationMailer
     @user = user
     @enrolment = enrolment
     @url  = 'http://tmg.edu.au'
-    mail(to: @user.email, subject: 'Welcome to Tmg Enrolment')
+    mail(to: @user.email, subject: 'Tmg Enrolment Summary')
   end
 end
