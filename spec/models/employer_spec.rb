428a338b6852a2b1952b085701096979cc31270c
Abhishek Ahirwar <abhiahirwar@gmail.com>
2016-10-24 13:44:30 +1100

    fix suntax empoyer spec

--- spec/models/employer_spec.rb
+++ spec/models/employer_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Employer, type: :model do
   let(:employer) { build(:employer) }
 
   context 'validation' do
-    it{ is_expected.to validate_presence_of(:trading_name) }
-    it{ is_expected.to validate_presence_of(:contact_name) }
-    it{ is_expected.to validate_presence_of(:training_address) }
-    it{ is_expected.to validate_presence_of(:phone) }
+    it { is_expected.to validate_presence_of(:trading_name) }
+    it { is_expected.to validate_presence_of(:contact_name) }
+    it { is_expected.to validate_presence_of(:training_address) }
+    it { is_expected.to validate_presence_of(:phone) }
   end
 end
