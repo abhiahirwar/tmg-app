c4a3ee825bd80dcb9d69e4fa398aab291b6e0f53
Abhishek Ahirwar <abhiahirwar@gmail.com>
2017-01-17 14:02:29 +1100

    remove secrets file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,14 +42,7 @@ Rails.application.configure do
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
-  ENV["GMAIL_USERNAME"] = 'tmgit@tmg.edu.au'
-  ENV["GMAIL_PASSWORD"] = 'Tmg$$Routers6'
 
-  ENV["CLIENT_ID"] = '530079316263-8dcpqaoeg3hp3asga08t0th5q03o5lav.apps.googleusercontent.com'
-  ENV["CLIENT_SECRET"] = 'eVxNy3K7o88j55ta10pQ9Lx3'
-  ENV["ACCESS_TOKEN"] = 'ya29.lgEHGtQQerEtZgLzQlUtPMOqSeo7kC-fcOzhvrgRKZlTR_TrDIA0p6WLJR6LI4H27K37ZHzU6YQfoQ'
-  ENV["REFRESH_TOKEN"] =  '1/aElQiV7cm8pEnfYa4N_3YVOc6xY6LOkQEY0bXpPD-f1IgOrJDtdun6zK6XiATCKT'
-  ENV['GOOGLE_DRIVE_PUBLIC_FOLDER_ID'] = '0B4QFjHfJ3wAFfm9GY3NWdXlNX2dZQmQzRzZTOGtHSFJ6eVNUTEh0al9RUTk5aG1MT0E3ZFE'
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -63,5 +56,5 @@ Rails.application.configure do
     :host => "localhost",
     :port => 3000
   }
-  config.action_mailer.default_options = { from: "no-reply@tmg.edu.au" }
+  config.action_mailer.default_options = { from: "no-reply@rto.edu.au" }
 end
